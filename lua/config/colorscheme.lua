f5583f806df13ed37123be27a6506ba845dbb7b9
phuoc101 <nguyenthuanphuoc101@protonmail.com>
2022-12-23 17:23:34 +0700

    theme one darker

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,5 +1,5 @@
 local plugin = "colorscheme"
-local colorscheme = "onedark"
+local colorscheme = "onedarker"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
