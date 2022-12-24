a8ae888c24ae157d7047e1c234888d2a0a2c1fde
phuoc101 <nguyenthuanphuoc101@protonmail.com>
2022-12-24 16:25:36 +0700

    new shortcut for comment, cmp new prompt (shorter)

--- lua/config/hop.lua
+++ lua/config/hop.lua
@@ -1,10 +1,6 @@
-local status_ok, hop = pcall(require, "hop")
+local status_ok, _ = pcall(require, "hop")
 if not status_ok then
 	return
-end
-
-hop.config = function()
-	require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
 end
 
 vim.keymap.set("n", "F", "<cmd>HopChar1<CR>", { desc = "Hop character" })
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -122,6 +122,10 @@ return packer.startup(function(use)
 	use({
 		"phaazon/hop.nvim",
 		branch = "v1", -- optional but strongly recommended
+		config = function()
+			-- you can configure Hop the way you like here; see :h hop-config
+			require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
+		end,
 	})
 
 	-- Code documentation
