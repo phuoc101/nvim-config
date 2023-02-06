1badacceb7e51690a04da35c2c2d81de7746bcd1
phuoc101 <nguyenthuanphuoc101@protonmail.com>
2023-02-06 17:56:53 +0200

    changed <leader>q to save and quit, map quit to <leader>Q, added alive_bar snip

--- lua/config/whichkey.lua
+++ lua/config/whichkey.lua
@@ -86,7 +86,8 @@ local main_mappings = {
   },
   ["e"] = { "<cmd>NvimTreeToggle<cr>", "Explorer" },
   ["w"] = { "<cmd>w!<CR>", "Save" },
-  ["q"] = { "<cmd>q!<CR>", "Quit" },
+  ["q"] = { "<cmd>wq<CR>", "Save and Quit" },
+  ["Q"] = { "<cmd>q!<CR>", "Quit" },
   ["c"] = { "<cmd>Bdelete!<CR>", "Close Buffer" },
   ["h"] = { "<cmd>nohlsearch<CR>", "No Highlight" },
   ["f"] = {
